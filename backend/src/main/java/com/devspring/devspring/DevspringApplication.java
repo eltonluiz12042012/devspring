f0c9c13c5b8db175f728c8035916fd268c73b130
Elton Luiz <eltonluiz12042012@gmail.com>
2024-05-15 14:22:08 -0300

    primeiro commit

---
+++ backend/src/main/java/com/devspring/devspring/DevspringApplication.java
@@ -0,0 +1,13 @@
+package com.devspring.devspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevspringApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devspring/devspring/DevspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devspring.devspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
